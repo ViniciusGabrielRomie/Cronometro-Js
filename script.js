b75fb49da97d280e934165ba3377b991db641c14
ViniciusGabrielRomie <130675071+ViniciusGabrielRomie@users.noreply.github.com>
2023-11-07 12:59:47 -0300

    Update script.js
    Finalizado script, falta testar e melhorar css e html

--- script.js
+++ script.js
@@ -12,4 +12,34 @@ window.onaload = function(){
             clearInterval(intervalo);
             intervalo = setInterval(startTimer, 10);
         }
+
+      botaoStop.onclick = function(){
+        clearInterval(intervalo);
+      }
+
+      botaoReset.onclick = fuction(){
+        clearInterval(intervalo);
+        segundos = 00;
+        milissegundos = 00;
+        acressentarSegundos.innerHtml = segundos;
+        acressentarMilissegundos.innerHtml = milissegundos;
+      }
+
+    fucction startTimer(){
+      milissegundos++;
+      if (milissegundos <= 9){
+        acressentarMilissegundos.innerHtml = "0" + milissegundos;
+      }
+      if (milissegundos >9){
+        acressentarMilissegundos.innerHtml = milissegundos;
+      }
+      
+      if (milissegundos>99){
+        segundos++;
+        milissegundos = 00;
+        acressentarSegundos.innerHtml = "0" + segundos;
+        milissegundos = 0;
+        acressentarMilissegundos.innerHtml = "0" + milissegundos;
+      }
+    }
 }
