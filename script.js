b16f7771b8e726f29788b33f06beb4c384ee6083
ViniciusGabrielRomie <130675071+ViniciusGabrielRomie@users.noreply.github.com>
2023-11-07 18:04:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
-window.onaload = function(){
-    var segundos = 00;
-    var milissegundos = 00;
-    var acressentarSegundos = document.getElementById('segundos');
-    var acressentarMilissegundos = document.getElementById('Milissegundos');
+window.onload = function(){
+    var segundos = 0;
+    var milissegundos = 0;
+    var acressentarSegundos = document.getElementById("Segundos");
+    var acressentarMilissegundos = document.getElementById("Milissegundos");
     var botaoStart = document.getElementById('Start');
     var botaoStop = document.getElementById('Stop');
     var botaoReset = document.getElementById('Reset');
-    var intervalo;
+    var Interval;
 
         botaoStart.onclick = function() {
-            clearInterval(intervalo);
-            intervalo = setInterval(startTimer, 10);
+            clearInterval(Interval);
+            Interval = setInterval(startTimer, 10);
         }
 
       botaoStop.onclick = function(){
-        clearInterval(intervalo);
+        clearInterval(Interval);
       }
 
-      botaoReset.onclick = fuction(){
-        clearInterval(intervalo);
-        segundos = 00;
-        milissegundos = 00;
-        acressentarSegundos.innerHtml = segundos;
-        acressentarMilissegundos.innerHtml = milissegundos;
+      botaoReset.onclick = function(){
+        clearInterval(Interval);
+        segundos = "00";
+        milissegundos = "00";
+        acressentarSegundos.innerHTML = segundos;
+        acressentarMilissegundos.innerHTML = milissegundos;
       }
 
-    fucction startTimer(){
+    function startTimer() {
       milissegundos++;
-      if (milissegundos <= 9){
-        acressentarMilissegundos.innerHtml = "0" + milissegundos;
+
+      if(milissegundos <= 9){
+        acressentarMilissegundos.innerHTML = "0" + milissegundos;
       }
-      if (milissegundos >9){
-        acressentarMilissegundos.innerHtml = milissegundos;
+      if (milissegundos > 9){
+        acressentarMilissegundos.innerHTML = milissegundos;
       }
       
       if (milissegundos>99){
         segundos++;
-        milissegundos = 00;
-        acressentarSegundos.innerHtml = "0" + segundos;
         milissegundos = 0;
-        acressentarMilissegundos.innerHtml = "0" + milissegundos;
+        acressentarSegundos.innerHTML = "0" + segundos;
+        milissegundos = 0;
+        acressentarMilissegundos.innerHTML = "0" + milissegundos;
+      }
+
+      if (segundos > 9){
+        acressentarSegundos.innerHTML = segundos;
       }
     }
 }
